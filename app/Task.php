1b68589509e6a82857fb94502472f48fb5c383ef
Firass Ziedan <firass.ziedan@gmail.com>
2018-06-09 01:54:42 +0300

    Add some eloquent querys 2

--- app/Task.php
+++ app/Task.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    public function isComplete() {
-      return false;
+    // Query Scopes
+    public function scopeIncomplete ($query) {
+      return $query->where('completed',0); //App\Task::incomplete()->get();
+
     }
 }
