99645cfb27af7bd01c583f7631d6c90e56530a67
Firass Ziedan <firass.ziedan@gmail.com>
2018-06-19 05:54:34 +0300

    Add Comments section and turn it on

--- app/Comment.php
+++ app/Comment.php
@@ -2,8 +2,9 @@
 
 namespace App;
 
-
 class Comment extends Model
 {
-    //
+  public function post() {
+    return $this->belongsTo(Post::class);
+  }
 }
--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,16 @@ namespace App;
 class Post extends Model
 {
     public function comments() {
-        return $this->hasmany(Comment::class);
+      return $this->hasmany(Comment::class);
+    }
+
+
+    public function addComment ($body) {
+      // Comment::create ([
+      //   'body' => $body,
+      //   'post_id' => $this->id
+      // ]);
+
+      $this->comments()->create(compact('body'));
     }
 }
