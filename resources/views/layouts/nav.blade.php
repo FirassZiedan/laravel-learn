6af13b0fc173ba591c2c4eaaff808a9dc979775d
Firass Ziedan <firass.ziedan@gmail.com>
2019-11-09 21:11:59 +0200

    Remove old all files and code

--- resources/views/layouts/nav.blade.php
+++
@@ -1,9 +0,0 @@
-<header class="masthead mb-auto">
-  <div class="inner">
-    <h3 class="masthead-brand">Posts</h3>
-    <nav class="nav nav-masthead justify-content-center">
-      <a class="nav-link active" href="/">Home</a>
-      <a class="nav-link" href="/tasks">Tasks</a>
-    </nav>
-  </div>
-</header>
